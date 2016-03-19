2c671003ba1e94b41b23836e645494ee4e91da0f
Rogery Arthur <a.pendragon@hotmail.fr>
2016-03-19 19:22:23 +0100

    modification des valeurs de control et mise en place du trigger sur la machine

---
+++ Assets/Upperground/Scripts/triggerMachine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerMachine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+    }
+}

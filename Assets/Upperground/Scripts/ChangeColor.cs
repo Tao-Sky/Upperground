a0fa07ad4e59da7f397d1ca1f87a26837b0411f2
Simon Galand <simon.galand@gmail.com>
2016-03-21 21:19:34 +0100

    Correction vis à vis du trigger machine

---
+++ Assets/Upperground/Scripts/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+	public Material M;
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.GetComponent<Rigidbody2D> ().position.x > -35.0) 
+		{
+			Debug.Log("coucou");
+		}
+	}
+}

a82b4d465d617225195528cfe2908b9c40b8b519
Simon Galand <simon.galand@gmail.com>
2016-04-02 18:57:22 +0200

    Animation de la porte

---
+++ Assets/Upperground/Scripts/nextLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetButtonDown("X button"))
+        {
+            Application.LoadLevel("Scene_1");
+        }
+    }
+}

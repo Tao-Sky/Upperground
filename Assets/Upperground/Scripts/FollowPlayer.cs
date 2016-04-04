26d0277c4fbfccf052fdda501839bf92039aad44
Hugo Lafon <hugo.lafon2@gmail.com>
2016-04-04 16:00:35 +0200

    Ajout du script de suivi de Lumo par Sha (à compléter).

---
+++ Assets/Upperground/Scripts/FollowPlayer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;//set target from inspector instead of looking in Update
+    public float speed = 4.0f;
+    public float basicSpeed = 4.0f;
+    public float minDistanceFromPlayer = 1;
+
+    private float lastDist;
+    private float dist;
+    private Vector3 lastDestination;
+
+    bool inPlayerRadius = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, target.position);
+
+        if (!inPlayerRadius && dist < 10)
+        {
+            MoveTowardsPlayer();
+        }
+
+        lastDist = dist;
+        lastDestination = transform.forward;
+    }
+
+    public void MoveRandom()
+    {
+        float f = Random.Range(0.1f, 0.2f);
+        if (Random.value > 0.5)
+            f *= -1;
+
+        Debug.Log(target.position);
+        transform.LookAt(new Vector3(lastDestination.x + f, lastDestination.y + f, 0));
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+        //Vector3 newVector = new Vector3((float)(Random.value - 0.5) * speed * Time.deltaTime, 0, 0);
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+
+    public void MoveTowardsPlayer()
+    {
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        if (dist > 5)
+        {
+            speed *= 1.01f;
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+
+        if (dist > minDistanceFromPlayer && dist <= 5)
+        {
+            if (dist <= lastDist)
+                speed *= 0.975f;
+
+            if (speed < basicSpeed)
+                speed = basicSpeed;
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    public void setInPlayerRadius(bool b)
+    {
+        inPlayerRadius = b;
+    }
+}

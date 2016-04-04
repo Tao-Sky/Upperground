296622bf0c93e026018d08271f9dcb9a0788f5a4
Hugo Lafon <hugo.lafon2@gmail.com>
2016-04-05 00:23:27 +0200

    Ennemi fonctionnel + triggers checkpoint + triggers déplacements ennemi

---
+++ Assets/Upperground/Scripts/TriggerCheckpoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCheckpoint : MonoBehaviour {
+
+    public bool isActivated = false;
+    //public bool firstTimeChecked = false;
+
+    public bool getIsActivated(){return isActivated;}
+    public void setIsActivated(bool b) {isActivated = b;}
+    //public bool getFirstTimeChecked() { return firstTimeChecked; }
+    //public void setFirstTimeChecked(bool b) { firstTimeChecked = b; }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach(Transform t in transform.parent)
+        {
+            t.GetComponent<TriggerCheckpoint>().setIsActivated(false);
+        }
+
+        //if (!getFirstTimeChecked())
+        //{
+        //  setFirstTimeChecked(true);
+            isActivated = true;
+        //}
+
+        //Debug.Log(isActivated);
+    }
+
+}

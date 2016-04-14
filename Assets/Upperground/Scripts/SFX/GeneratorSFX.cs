432919577dc73187e8b68c8de79d5bae9918e221
Simon Galand <simon.galand@gmail.com>
2016-04-14 17:32:57 +0200

    Animation du générateur ralentie + début de bruitages de pas pour Lumo

---
+++ Assets/Upperground/Scripts/SFX/GeneratorSFX.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorSFX : MonoBehaviour {
+	public AudioSource SFX;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		GameObject Generator = GameObject.Find("Generator");
+		int rand = Random.Range (85, 120);
+		float pitch = (float)rand / 100.0f;
+		if(Generator.GetComponent<SpriteRenderer>().sprite.name == "broken")
+		{
+			SFX.pitch = pitch;
+			SFX.Play ();
+		}
+	}
+}
---
+++ Assets/Upperground/Scripts/SFX/LumoSFX.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LumoSFX : MonoBehaviour {
+	public AudioSource Run;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GameObject Player = GameObject.Find("Player");
+		int rand = Random.Range (75, 105);
+		float pitch = (float)rand / 100.0f;
+		if(Player.GetComponent<Animator>().GetFloat("speed")>0.2 && Player.GetComponent<Animator>().GetBool("grounded") && !Run.isPlaying)
+		{
+			Run.pitch = pitch;
+			Run.Play ();
+		}
+		else if(Player.GetComponent<Animator>().GetFloat("speed")<0.2 || !Player.GetComponent<Animator>().GetBool("grounded") )
+		{
+			Run.Stop ();
+		}
+	}
+}

805f402f0f59bde9f1bac3248d07ba821448bba4
Hugo Lafon <hugo.lafon2@gmail.com>
2016-04-26 00:41:33 +0200

    Ajout d'un nouvel ennemi : respawn après 5 secondes de mort. A prévoir: saut augmenté en rebondissant sur l'ennemi mort.

--- Assets/Upperground/Scripts/EnemyFight.cs
+++ Assets/Upperground/Scripts/EnemyFight.cs
@@ -6,6 +6,15 @@ public class EnemyFight : MonoBehaviour
     public SpriteRenderer healthBar;
     public GameObject particuleDetection;
 
+    private SpriteRenderer sr;
+    private BoxCollider2D box2D;
+    private Animator anim;
+    
+    public GameObject deadEnemy;
+    public int enemyType;
+
+    private bool noCoroutine = true; 
+
     public float totalHealth = 3;
     private float currentHealth;
 
@@ -13,6 +22,18 @@ public class EnemyFight : MonoBehaviour
     {
         healthBar.enabled = false;
         currentHealth = totalHealth;
+
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        box2D = GetComponent<BoxCollider2D>();
+
+        if (enemyType == 1)
+            deadEnemy = null;
+        else if (enemyType == 2)
+        {
+            deadEnemy.GetComponent<SpriteRenderer>().enabled = false;
+            deadEnemy.GetComponent<BoxCollider2D>().enabled = false;
+        }  
     }
 
     void Update()
@@ -26,12 +47,18 @@ public class EnemyFight : MonoBehaviour
         if (currentHealth > 0)
             currentHealth--;
 
-        healthBar.transform.localScale = new Vector3(currentHealth / totalHealth, 1, 1);
+        healthBar.transform.localScale = new Vector3(currentHealth / totalHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
     }
 
     public float getHealthPoints()
     {
+        Debug.Log(currentHealth);
         return currentHealth;
+    }
+
+    public int getEnemyType()
+    {
+        return enemyType;
     }
 
     public void showHealthBar(bool b)
@@ -49,7 +76,65 @@ public class EnemyFight : MonoBehaviour
 
     public void EnemyDie()
     {
-        //Faire clignoter le sprite
-        Destroy(this.gameObject);
+        if (enemyType == 1)
+            Destroy(this.gameObject);
+
+        else if (enemyType == 2)
+        {
+            StartCoroutine(deathCoroutine(5.0f));
+        }
+    }
+
+    IEnumerator deathCoroutine(float time)
+    {
+        noCoroutine = false;
+
+        anim.enabled = false;
+        box2D.enabled = false;
+        sr.enabled = false;
+        particuleDetection.SetActive(false);
+        
+        deadEnemy.GetComponent<SpriteRenderer>().enabled = true;
+        deadEnemy.GetComponent<BoxCollider2D>().enabled = true;
+        transform.tag = "Untagged";
+    
+        yield return new WaitForSeconds(time);
+
+        deadEnemy.GetComponent<SpriteRenderer>().enabled = false;
+        deadEnemy.GetComponent<BoxCollider2D>().enabled = false;
+    
+        box2D.enabled = true;
+        sr.enabled = true;
+        anim.enabled = true;
+        transform.tag = "Enemy";
+
+        currentHealth = totalHealth;
+        healthBar.transform.localScale = new Vector3(currentHealth/totalHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+        healthBar.enabled = false;
+
+        noCoroutine = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" && transform.tag == "Enemy")
+        {
+            GameObject[] listeCP = GameObject.FindGameObjectsWithTag("Checkpoint");
+
+            foreach (GameObject g in listeCP)
+            {
+                if (g.GetComponent<TriggerCheckpoint>().getIsActivated() == true)
+                {
+                    coll.gameObject.GetComponent<PlayerController>().isRespawning = true;
+                    coll.gameObject.GetComponent<PlayerController>().getRigidbody2D().velocity = new Vector2(0, 0);
+                    coll.transform.position = new Vector3(g.transform.position.x, g.transform.position.y - 4.0f, coll.transform.position.z);
+                    GameObject s = GameObject.Find("Sha");
+                    if (s.GetComponent<FollowPlayer>().playerFound == true && s.GetComponent<FollowPlayer>().nocoroutine == true)
+                        s.transform.position = new Vector3(g.transform.position.x, g.transform.position.y - 4.0f, s.transform.position.z);
+
+                    break;
+                }
+            }
+        }
     }
 }

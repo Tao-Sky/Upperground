ef0785c85a339be47be768aa629e2085f8b761fd
Simon Galand <simon.galand@gmail.com>
2016-05-08 16:10:46 +0200

    Quelques réglages de volume (pas terminé) + un sprite de porte de deuxième niveau et background

--- Assets/Upperground/Scripts/dialogueposition.cs
+++ Assets/Upperground/Scripts/dialogueposition.cs
@@ -5,6 +5,6 @@ public class dialogueposition : MonoBehaviour {
 
     public GameObject target;
 	void Update () {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y+1f,-1.5f);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y+1f,-1.6f);
 	}
 }
